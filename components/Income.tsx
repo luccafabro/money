d765daacda2137377ec501e933417dade8fa696a
JGZimmermann <jegiannini01@gmail.com>
2023-04-13 14:27:40 -0300

    Página de Receita funcionando

--- components/Income.tsx
+++ components/Income.tsx
@@ -1,44 +1,64 @@
-import { Center ,NativeBaseProvider, Box, HStack, VStack, Text, extendTheme, Avatar, Badge, Fab, Icon, FlatList } from 'native-base';
-import React from 'react'
-import { Link } from 'expo-router'
-import { AntDesign } from "@expo/vector-icons";
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, FlatList, StyleSheet } from 'react-native';
 
-export default function Income() {
-  return (
-    <NativeBaseProvider theme={theme}>
-        <FlatList data={data} renderItem={({
-            item
-        }) =>
-            <Text>{item.name}</Text>
-        
-        }
-        />
-    </NativeBaseProvider>
-  )
+interface Item {
+  id: string;
+  nome: string;
+  valor: string;
 }
 
-const data = [
-    {
-        name:"Salário",
-        description:"Quantia mensal que recebo em troca do meu serviço",
-        value:"R$ 5000"
-    },
-    {
-        name:"Rendimentos",
-        description:"Valor que recebo dos meus investimentos",
-        value:"R$ 350"
-    },
-    {
-        name:"Freelance",
-        description:"Serviço sem contrato que faço para aumentar minha renda",
-        value:"R$ 740"
-    },
-]
+export default function App() {
+  const [nome, setNome] = useState('');
+  const [valor, setValor] = useState('');
+  const [lista, setLista] = useState<Item[]>([]);
 
-const theme = extendTheme({
-    colors: {
-      primary: {
-        50: '#150068ff'
-      }
-    }
-  })
+  const handleAddItem = () => {
+    setLista([...lista, { id: Math.random().toString(), nome, valor }]);
+    setNome('');
+    setValor('');
+  };
+
+  return (
+    <View style={{ flex: 1, padding: 30 }}>
+      <View style={{ flexDirection: 'row' }}>
+        <View style={{ flex: 1 }}>
+          <Text style={{ fontSize: 16, fontWeight: 'bold' }}>Nome:</Text>
+          <TextInput
+            style={{ borderWidth: 1, padding: 5 }}
+            value={nome}
+            onChangeText={setNome}
+          />
+        </View>
+        <View style={{ flex: 1, marginLeft: 10, marginBottom: 20 }}>
+          <Text style={{ fontSize: 16, fontWeight: 'bold' }}>Valor:</Text>
+          <TextInput
+            style={{ borderWidth: 1, padding: 5 }}
+            keyboardType="numeric"
+            value={valor}
+            onChangeText={setValor}
+          />
+        </View>
+      </View>
+      <Button color='#150068ff' title="Adicionar" onPress={handleAddItem}  />
+      <View style={{ marginTop: 20 }}>
+        <FlatList
+          data={lista}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <View style={{ flexDirection: 'row', marginVertical: 5 }}>
+              <Text style={{ flex: 1 }}>{item.nome}</Text>
+              <Text style={{ flex: 1, textAlign: 'right' }}>{item.valor}</Text>
+            </View>
+          )}
+        />
+      </View>
+    </View>
+  );
+}
+
+const style = StyleSheet.create({
+  button: {
+    marginTop: 40,
+    backgroundColor: '#150068ff',
+  },
+});
